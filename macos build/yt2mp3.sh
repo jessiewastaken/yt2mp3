61e3f9a9fd95d8f815277c24d49ab0adf829e997
Jessie Miall <90266062+jessiewastaken@users.noreply.github.com>
2023-08-11 02:40:12 +0100

    Update yt2mp3.sh

--- macos build/yt2mp3.sh
+++ macos build/yt2mp3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read 'Enter a YouTube URL (that is not from a playlist): ' url
+read -p 'Enter a YouTube URL (that is not from a playlist): ' url
 
 ./yt-dlp_macos $url -P ~/Music -o %%(title)s.%%(ext)s --embed-thumbnail --embed-metadata -x --audio-format mp3
 
